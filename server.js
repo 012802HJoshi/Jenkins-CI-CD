412d211158d61282c5746a37ed13a8f4a2b2c6f5
Harshit Joshi <012802HJoshi@users.noreply.github.com>
2026-04-24 17:40:59 +0530

    Jenkins test

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ ok: true, message: "Fitness Exercises API V2" });
+  res.json({ ok: true, message: "Fitness Exercises API V2.1" });
 });
 
 app.use("/api/exercises", exerciseRoutes);
